824cf27b6c0f05c11265e71a2baff8bf1c5d7084
Oreder <stepup.ored@gmail.com>
2019-03-18 00:50:00 +0300

    Property binding. String interpolation. Event binding: passing and using data in first method ($event)

--- demo-app/src/app/servers/servers.component.ts
+++ demo-app/src/app/servers/servers.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-servers',
-  //selector: '[app-servers]',
-  selector:'.app-servers',
+  selector: 'app-servers',      // type #1: typescript optimal
+  //selector: '[app-servers]',  // type #2: name for tag
+  //selector:'.app-servers',    // type #3: name for class
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "";
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;   
+    }, (2000));
+  }
 
   ngOnInit() {
   }
 
+  onClickServerCreation(){
+    this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
