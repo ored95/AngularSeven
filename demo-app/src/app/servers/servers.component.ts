262bd17417de0ea110e25b0bf8af509255e7799d
Oreder <stepup.ored@gmail.com>
2019-04-08 16:35:26 +0300

    View encapsulation properties: None, Emulated, Native, ShadowDom

--- demo-app/src/app/servers/servers.component.ts
+++ demo-app/src/app/servers/servers.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-servers',      // type #1: typescript optimal
   //selector: '[app-servers]',  // type #2: name for tag
   //selector:'.app-servers',    // type #3: name for class
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class ServersComponent implements OnInit {
