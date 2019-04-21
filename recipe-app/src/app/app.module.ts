fdcab62a51d422d8af513f7d73cbf14a3f52ae28
Oreder <stepup.ored@gmail.com>
2019-04-21 22:06:28 +0300

    Course Project: Building and Using a Dropdown Directive

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { HeaderComponent } from './header/header.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { HeaderComponent } from './header/header.component';
     ShoppingListComponent,
     ShoppingEditComponent,
     HeaderComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
